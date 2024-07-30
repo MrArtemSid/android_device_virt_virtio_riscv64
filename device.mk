be7f68b41a7d6af4032f8e2177ce70f4fbec04a3
MrArtemSid <artem.sid.key@gmail.com>
2024-07-30 16:11:03 +0300

    virtio_riscv64: Adapt for RISC-V

--- device.mk
+++ device.mk
@@ -6,11 +6,14 @@
 
 # Kernel (Prebuilt)
 TARGET_PREBUILT_EMULATOR_KERNEL_USE ?= 6.6
-TARGET_PREBUILT_KERNEL_ARCH := arm64
-TARGET_PREBUILT_KERNEL_MODULES_ARCH := arm64
+TARGET_PREBUILT_KERNEL_ARCH := riscv64
+TARGET_PREBUILT_KERNEL_MODULES_ARCH := riscv64
 
 # Inherit from common
 $(call inherit-product, device/virt/virtio-common/device-common.mk)
+
+PRODUCT_VENDOR_PROPERTIES += \
+    config.disable_renderscript=1
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
